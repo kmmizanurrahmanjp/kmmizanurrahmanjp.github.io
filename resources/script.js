8d1caf09f987d7889aa6e13d1e2cb89ac4080507
kmmizanurrahmanjp <https.mizan@gmail.com>
2018-09-02 06:31:41 +0600

    Add about part

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-})
+function onPageLoding(){
+    //Ereperience Part
+    //medisys exeperience
+    var medisysJoinDate = new Date(2017, 03, 01); //year, month, and day
+    var leavindDate = new Date();
+    var timeDiff = Math.abs(leavindDate.getTime() - medisysJoinDate.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    var medisysYearsMath = diffDays/365;
+    var medisysExeperience = medisysYearsMath.toString().slice(0, 3);
+    document.getElementById('medisys_expe').innerHTML= "Experience: "+medisysExeperience+" Years";
+
+    //Total erperence
+    var totalExperience = medisysExeperience
+    document.getElementById('total_expe').innerHTML= "Experience "+totalExperience+" Years+";
+    
+}
